a6176d123f58ec0d37671f70062c226a02129b5c
Tal Hayut <talha@accessibe.com>
2022-07-28 16:42:00 +0300

    feat: two sum 2

--- problems/twoSum2.js
+++ problems/twoSum2.js
@@ -3,12 +3,9 @@ var twoSum = function (numbers, target) {
   for (let i = 0; i < numbers.length; i++) {
     const diff = target - numbers[i];
     if (diff in map) {
-      return [map[diff], i];
+      return [map[diff], i + 1];
     } else {
       map[numbers[i]] = i + 1;
     }
   }
 };
-
-
-console.log(twoSum([2,7,11,15], 9));
